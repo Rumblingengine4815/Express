1a2cbec7ebef2dd341156e224b81f94add2b63f3
Lorion Ravindradasan <lorionsurjith@gmail.com>
2025-08-24 23:04:04 +0530

    Added corrected files

---
+++ src/routes/greeting.route.ts
@@ -0,0 +1,29 @@
+import { Router,Request,Response } from "express";
+
+export class GreetingRoute{
+    private static instance:GreetingRoute;
+
+    public static getInstance():GreetingRoute{
+        if(!GreetingRoute.instance){
+            GreetingRoute.instance = new GreetingRoute();
+        }
+        return GreetingRoute.instance;
+    }
+    
+    public router:Router;
+    private constructor(){
+        this.router = Router();
+        this.setupRoutes();
+    }
+
+    private setupRoutes(){
+        this.router.get("/", (req:Request, res:Response) => {
+            res.send("Hello World");
+        });
+
+        this.router.post("/", (req:Request, res:Response) => {
+            const {name} = req.body;
+            res.send(`Hello ${name}`);
+        });
+    }
+}
