2923762a29511d47d91bd4aac925c699a5fcb872
Lorion Ravindradasan <lorionsurjith@gmail.com>
2025-08-26 19:53:00 +0530

    pushed backend 1st part

---
+++ src/common/constants/errors.constants.ts
@@ -0,0 +1,22 @@
+export const ERRORS = {
+    BAD_REQUEST : {
+        key: 'BAD_REQUEST',
+        message: 'Invalid syntax or missing parameters'
+    },
+    UNAUTHORIZED : {
+        key: 'UNAUTHORIZED',
+        message: 'You are not allowed to access this resource'
+    },
+    LOGIN_FAILED : {
+        key: 'LOGIN_FAILED',
+        message: 'Invalid username or password'
+    },
+    GET_FAILED:{
+        key: 'GET_FAILED',
+        message: 'Failed to get the data'
+    },
+    USER_NOT_FOUND : {
+        key: 'USER_NOT_FOUND',
+        message: 'User not found'
+    }
+}
